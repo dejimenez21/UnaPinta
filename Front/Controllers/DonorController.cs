cdf39d920eff9af6705744c0d93e9cf65a506b1c
Abraham Morillo <abrahammorillo777@gmail.com>
2021-05-24 21:41:26 -0400

    adding some files to create some pages

---
+++ Front/Controllers/DonorController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Una_Pinta.Controllers
+{
+    public class DonorController : Controller
+    {
+        public IActionResult DonorIndexPage()
+        {
+            return View();
+        }
+
+        public IActionResult DonorRegisterPage()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult DonorTapRegister()
+        {
+            return View();
+        }
+    }
+}
