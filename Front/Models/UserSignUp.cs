3eb2b8154c6ac4e984a717d579b73328203aec34
Abraham Morillo <abrahammorillo777@gmail.com>
2021-06-07 20:32:59 -0400

    removing references

---
+++ Front/Models/UserSignUp.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Una_Pinta.Models
+{
+    public class UserSignUp
+    {
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        [Required]
+        public bool Sex { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime? BirthDate { get; set; }
+
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [DataType(DataType.PhoneNumber)]
+        public string PhoneNumber { get; set; }
+
+        [Required]
+        public string UserName { get; set; }
+
+        [Required]
+        [MinLength(8)]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required]
+        public string Role { get; set; }
+
+        public int? BloodTypeId { get; set; }
+
+        public double? Weight { get; set; }
+    }
+}
