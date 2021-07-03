1c94cd94ff056af0f3b1f72896a5997890761cc1
Abraham Morillo <abrahammorillo777@gmail.com>
2021-07-03 08:07:13 -0400

    ADD donor register form

--- Front/Controllers/DonorController.cs
+++ Front/Controllers/DonorController.cs
@@ -4,11 +4,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Una_Pinta.Models;
+using Una_Pinta.Services;
 
 namespace Una_Pinta.Controllers
 {
     public class DonorController : Controller
     {
+        readonly IUserRepository _userRepository;
+        const string registerpage = "DonorRegisterPage";
+
+        public DonorController(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
         public IActionResult DonorIndexPage()
         {
             return View();
@@ -22,7 +31,8 @@ namespace Una_Pinta.Controllers
         [HttpPost]
         public IActionResult DonorTapRegister(UserSignUp userSignUp)
         {
-            return View();
+            _userRepository.PostUser(userSignUp);
+            return View(registerpage);
         }
 
         public IActionResult DonorQuestionsPage()
