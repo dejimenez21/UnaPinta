b176b6b0116b4f23664bb53a2d1fab64e49890e1
DANIEL ELIAS JIMENEZ ROJAS <dejimenez21@gmail.com>
2021-07-26 23:29:11 -0400

    Migrate BloodTypeDictionary to shared project

--- UnaPinta.Core/Services/RequestsService.cs
+++ UnaPinta.Core/Services/RequestsService.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using UnaPinta.Data.Entities;
-using UnaPinta.Core.Helpers;
+using UnaPinta.Dto.Helpers;
 using UnaPinta.Data.Contracts;
 using UnaPinta.Core.Contracts;
 using Microsoft.AspNetCore.Identity;
