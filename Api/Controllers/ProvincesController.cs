392908aafc1943999581e697f40656b4a18bd2af
DANIEL ELIAS JIMENEZ ROJAS <dejimenez21@gmail.com>
2021-08-13 23:53:51 -0400

    Add ProvincesController and add endpoint to get all provinces

---
+++ Api/Controllers/ProvincesController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UnaPinta.Core.Contracts;
+using UnaPinta.Data.Entities;
+
+namespace UnaPinta.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProvincesController : ControllerBase
+    {
+        private readonly IProvinceService _provinceService;
+
+        public ProvincesController(IProvinceService provinceService)
+        {
+            _provinceService = provinceService;
+        }
+
+        public async Task<ActionResult<IEnumerable<Province>>> GetAllProvinces()
+        {
+            var provinces = await _provinceService.GetAllProvinces();
+            return Ok(provinces);
+        }
+
+    }
+}
