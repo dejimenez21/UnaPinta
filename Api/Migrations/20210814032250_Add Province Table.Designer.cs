d88388b04bae805e2deb0b9ed787605f4e2f3bc4
DANIEL ELIAS JIMENEZ ROJAS <dejimenez21@gmail.com>
2021-08-13 23:28:55 -0400

    Add province table creation migration

--- Api/Migrations/20210814032250_Add Province Table.Designer.cs
+++ Api/Migrations/20210814032250_Add Province Table.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using UnaPinta.Data.Entities;
 
 namespace UnaPinta.Api.Migrations
 {
     [DbContext(typeof(UnaPintaDBContext))]
-    partial class UnaPintaDBContextModelSnapshot : ModelSnapshot
+    [Migration("20210814032250_Add Province Table")]
+    partial class AddProvinceTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
