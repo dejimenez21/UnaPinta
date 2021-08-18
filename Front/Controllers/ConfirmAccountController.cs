a271cf226e8c26433e348e2ccef7abf1888124e8
Abraham Morillo <abrahammorillo777@gmail.com>
2021-08-17 21:30:41 -0400

    page to confirm account was added

---
+++ Front/Controllers/ConfirmAccountController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Una_Pinta.Controllers
+{
+    public class ConfirmAccountController : Controller
+    {
+        public IActionResult ConfirmAccount()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult ConfirmButton()
+        {
+            return View();
+        }
+    }
+}
