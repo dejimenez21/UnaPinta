02f6270b80896a3152d380d15587594469e96717
DANIEL ELIAS JIMENEZ ROJAS <dejimenez21@gmail.com>
2021-08-25 00:29:20 -0400

    Fix requestcreate -> request mapping

--- Api/Controllers/RequestsController.cs
+++ Api/Controllers/RequestsController.cs
@@ -10,7 +10,7 @@ namespace UnaPinta.Api.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    [Authorize]
+    [Authorize(Roles = "solicitante")]
     public class RequestsController : ControllerBase
     {
         private readonly IMapper _mapper;
