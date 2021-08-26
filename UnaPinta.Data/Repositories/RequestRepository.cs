d1128613d47423138185a8c84d57c57d676db29f
DANIEL ELIAS JIMENEZ ROJAS <dejimenez21@gmail.com>
2021-08-26 00:09:18 -0400

    Add son feature files

---
+++ UnaPinta.Data/Repositories/RequestRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnaPinta.Data.Contracts;
+using UnaPinta.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace UnaPinta.Data.Repositories
+{
+    public class RequestRepository : IRequestRepository
+    {
+        private readonly UnaPintaDBContext _dbContext;
+
+        public RequestRepository(UnaPintaDBContext dbContext)
+        {
+            _dbContext = dbContext;   
+        }
+        public async Task<Request> SelectRequestById(int id)
+        {
+            return await _dbContext.Requests.SingleOrDefaultAsync(r => r.Id == id);
+        }
+    }
+}
---
+++ UnaPinta.Dto/Models/RequestDetailsDto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnaPinta.Dto.Models
+{
+    public class RequestDetailsDto
+    {
+        public int Id { get; set; }
+        public string RequesterName { get; set; }
+        public string RequesterEmail { get; set; }
+        public string RequesterPhone { get; set; }
+        public string PatientName { get; set; }
+        public string PatientPhone { get; set; }
+        public string CenterName { get; set; }
+        public string CenterAddress { get; set; }
+        public string BloodComponent { get; set; }
+        public string BloodType { get; set; }
+        public string Prescription { get; set; }
+    }
+}
