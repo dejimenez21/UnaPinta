ec7639b6b2308c2dce7090fcf22b8bd45b2ca409
Abraham Morillo <abrahammorillo777@gmail.com>
2021-09-13 19:50:18 -0400

    [REM] Commented lines

--- Front/wwwroot/scripts/donor.js
+++ Front/wwwroot/scripts/donor.js
@@ -11,37 +11,6 @@ function always(){
 }
 
 
-//function btnRegister_Click() {
-//    //alert("");
-//    var obj = {};
-
-//    obj.userId = 18;
-//    obj.conditions = [];
-
-//    obj.conditions.push({conditionId: $("#tattooQuestion").val(), months: $("#tattooCondition").val()});
-//    obj.conditions.push({conditionId: $("#bloodQuestion").val(), months: $("#bloodCondition").val()});
-//    obj.conditions.push({conditionId: $("#piercingQuestion").val(), months: $("#piercingCondition").val()});
-//    obj.conditions.push({conditionId: $("#transplanteQuestion").val(), months: -1});
-//    obj.conditions.push({conditionId: $("#drugQuestion").val(), months: $("#drugCondition").val()});
-//    obj.conditions.push({conditionId: $("#pregnanQuestion").val(), months: $("#pregnanCondition").val()});
-//    obj.conditions.push({conditionId: $("#transfusionQuestion").val(), months: $("#transfusionCondition").val()});
-//    obj.conditions.push({conditionId: $("#odonQuestion").val(), months: 1});
-//    obj.conditions.push({conditionId: $("#enfermedadesList").val(), months: -1});
-
-//    $.ajax({
-//        url: 'https://localhost:44393/api/waitlist',
-//        method: "POST",
-//        data: JSON.stringify(obj),
-//        dataType: "json",
-//        headers: {
-//            'Accept': 'application/json',
-//            'Content-Type': 'application/json' 
-//        }
-//      }).done(onSuccess)
-//        .fail(onError)
-//        .always(always);
-//}
-
 function btnRegister_Click() {
     var obj = {};
 
