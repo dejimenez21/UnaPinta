0d89ab258f29ed190795ca480f75b445a732a2e6
djimenezr <d.jimenezr@aduanas.gob.do>
2021-09-21 19:31:53 -0400

    Add mockEmailBroker methods implementation

--- UnaPinta.Data/Brokers/MockEmailBroker.cs
+++ UnaPinta.Data/Brokers/MockEmailBroker.cs
@@ -5,11 +5,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using UnaPinta.Data.Contracts;
+using System.IO;
 
 namespace UnaPinta.Data.Brokers
 {
     public class MockEmailBroker : IEmailBroker
     {
+        private readonly DirectoryInfo _dirInfo;
+
+        public MockEmailBroker()
+        {
+            _dirInfo = new DirectoryInfo(Environment.CurrentDirectory + "/EmailMocks/");
+        }
+
         public async Task Send(string message, MailboxAddress to)
         {
             
@@ -17,7 +25,19 @@ namespace UnaPinta.Data.Brokers
 
         public async Task Send(MailboxAddress to, string subject, MimeEntity body)
         {
-            
+            var path = _dirInfo.CreateSubdirectory($"{subject}-{DateTime.Now.Hour}");
+            var file = path.FullName + $"/{to.Name} - {to.Address}.html";
+            await body.WriteToAsync(file);
+        }
+
+        public async Task SendToMany(IEnumerable<MailboxAddress> to, string subject, MimeEntity body)
+        {
+            foreach(var dest in to)
+            {
+                var path = _dirInfo.CreateSubdirectory($"{subject}-{DateTime.Now}");
+                var file = path.FullName + $"{dest.Name} - {dest.Address}.html";
+                await body.WriteToAsync(file);
+            }  
         }
     }
 }
