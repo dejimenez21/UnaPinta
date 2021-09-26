f49f071db1aa9dc918a91d2d743d5880f19e9432
DANIEL ELIAS JIMENEZ ROJAS <dejimenez21@gmail.com>
2021-09-26 19:03:54 -0400

    [Remove] Send notification method from IRequestService

--- UnaPinta.Core/Contracts/IRequestsService.cs
+++ UnaPinta.Core/Contracts/IRequestsService.cs
@@ -7,7 +7,6 @@ namespace UnaPinta.Core.Contracts
 {
     public interface IRequestsService
     {
-        Task SendRequestNotification(Request request);
         Task<RequestDetailsDto> RetrieveRequestDetailsById(int id);
         Task<Func<Task>> CreateRequest(RequestCreate request, string userName);
     }
