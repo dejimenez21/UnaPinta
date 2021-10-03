60a063de35be12a84b0fbbb12c627060da456506
DANIEL ELIAS JIMENEZ ROJAS <dejimenez21@gmail.com>
2021-10-03 18:06:21 -0400

    [ADD] Case repository

---
+++ UnaPinta.Data/Repositories/CaseRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnaPinta.Data.Contracts;
+using UnaPinta.Data.Entities;
+
+namespace UnaPinta.Data.Repositories
+{
+    public class CaseRepository : RepositoryBase<Case>, ICaseRepository
+    {
+        public CaseRepository(UnaPintaDBContext dBContext) : base(dBContext) { }
+    }
+}
