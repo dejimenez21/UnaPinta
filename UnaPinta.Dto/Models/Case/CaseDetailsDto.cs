09ad2003803cbe6a5c23ef20d5594ccc866dd81d
DANIEL ELIAS JIMENEZ ROJAS <dejimenez21@gmail.com>
2021-10-08 08:07:48 -0400

    [ADD] CaseDetails, CreateCase and DonorInfo dtos

---
+++ UnaPinta.Dto/Models/Case/CaseDetailsDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnaPinta.Dto.Models.Donor;
+
+namespace UnaPinta.Dto.Models.Case
+{
+    public class CaseDetailsDto
+    {
+        public long Id { get; set; }
+        public string Status { get; set; }
+        public DonorInfoDto Donor { get; set; }
+        public RequestDetailsDto Request { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
