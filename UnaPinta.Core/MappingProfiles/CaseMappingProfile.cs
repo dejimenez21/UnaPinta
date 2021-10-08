2156c4ffc48d8d4dec58461a11885715cb97a0f9
DANIEL ELIAS JIMENEZ ROJAS <dejimenez21@gmail.com>
2021-10-08 08:08:55 -0400

    [ADD] New mapping profiles and reorganice existing ones

---
+++ UnaPinta.Core/MappingProfiles/CaseMappingProfile.cs
@@ -0,0 +1,39 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnaPinta.Data.Entities;
+using UnaPinta.Dto.Enums;
+using UnaPinta.Dto.Models;
+using UnaPinta.Dto.Models.Case;
+using UnaPinta.Dto.Models.Donor;
+
+namespace UnaPinta.Core.MappingProfiles
+{
+    public class CaseMappingProfile : Profile
+    {
+        public CaseMappingProfile()
+        {
+            #region To Entity
+            CreateMap<CreateCaseDto, Case>();
+            #endregion
+
+            #region To Dto
+            CreateMap<Case, CaseDetailsDto>().ConvertUsing((entity, dto, cfg) =>
+            {
+                if (dto == null) dto = new CaseDetailsDto();
+                dto.Id = entity.Id;
+                dto.CreatedAt = entity.CreatedAt;
+
+                dto.Status = entity.StatusNav.Description;
+                dto.Request = cfg.Mapper.Map<RequestDetailsDto>(entity.RequestNav);
+                dto.Donor = cfg.Mapper.Map<DonorInfoDto>(entity.DonorNav);
+
+                return dto;
+            });
+            #endregion
+        }
+    }
+}
---
+++ UnaPinta.Core/MappingProfiles/UserMappingProfile.cs
@@ -0,0 +1,35 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnaPinta.Data.Entities;
+using UnaPinta.Dto.Models;
+using UnaPinta.Dto.Models.Donor;
+
+namespace UnaPinta.Core.MappingProfiles
+{
+    public class UserMappingProfile : Profile
+    {
+        public UserMappingProfile()
+        {
+            #region To Entity
+            CreateMap<UserSignUp, User>();
+            #endregion
+
+            #region To Dto
+            CreateMap<User, DonorInfoDto>().ConvertUsing((entity, dto) =>
+            {
+                if (dto == null) dto = new DonorInfoDto();
+                dto.FullName = $"{entity.FirstName} {entity.LastName}";
+                dto.BloodType = entity.BloodTypeNav.Description;
+                dto.PhoneNumber = entity.PhoneNumber;
+                dto.Email = entity.Email;
+
+                return dto;
+            });
+            #endregion
+        }
+    }
+}
