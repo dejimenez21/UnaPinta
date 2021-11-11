9bcdd761e4a04ba64be5b4019170c81b958db7d1
Abraham Morillo <abrahammorillo777@gmail.com>
2021-11-10 21:59:53 -0400

    [ADD] Method to post request to unapinta api

--- Front/unapintaui/src/index.js
+++ Front/unapintaui/src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Login from './pages/login/login';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 ReactDOM.render(
@@ -12,6 +13,8 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
--- Front/unapintaui/src/pages/login/login.js
+++ Front/unapintaui/src/pages/login/login.js
@@ -1,20 +1,55 @@
-import React from 'react'
-import { Form } from 'react-bootstrap'
-import { Button } from 'react-bootstrap'
+import React from 'react';
+import axios from 'axios';
 
 class Login extends React.Component {
+    constructor(props){
+        super(props)
+
+        this.state = {
+            userName: '',
+            password: ''
+        }
+    }
+
+    changeHandler = (e) => {
+        this.setState({[e.target.name]: e.target.value})
+    }
+
+    submitHandler = (e) => {
+        e.preventDefault();
+        console.log(this.state);
+        axios.post("https://localhost:44393/api/Auth/login", this.state).then(response => {
+            console.log(response)
+        }).catch(error => {
+            console.log(error)
+        })
+        
+    }
+
     render(){
+        const{userName, password} = this.state
         return(
-            <div className="container">
-                <Form>
-                    <Form.Group>
-                        <Form.Control type="text" placeholder="Ingrese su nombre de usuario"/>
-                        <Form.Control type="password" placeholder="Ingrese su contraseña"/>
-                    </Form.Group>
-                <Button variant="primary" type="submit">
-                    Ingresar
-                </Button>
-                </Form>
+            <div className="container p-5">
+                <div className="card" style={{boxShadow: "0 4px 8px 0 rgba(0,0,0,0.2)", transition: "0.5s"}}>
+                <div className="card-header text-center">
+                    <p style={{fontfamily: "Roboto", fontSize:"30px", fontWeight: "Bold"}}>Inicio de Sesión</p>
+                </div>
+                    <div className="card-body">
+                        <form onSubmit={this.submitHandler}>
+                            <div className="mb-3">
+                                <input type="text" name="userName" value={userName} onChange={this.changeHandler} className="form-control" placeholder="Ingrese su nombre de usuario"/>
+                            </div>
+                            <br/>
+                            <div className="mb-3">
+                                <input type="password" name="password" value={password} onChange={this.changeHandler} className="form-control" placeholder="Ingrese su contraseña"/>
+                            </div>
+                            <br />
+                            <button className="btn btn-success" type="submit">
+                                Ingresar
+                            </button>
+                        </form>
+                    </div>
+                </div>
             </div>
         )
     }
