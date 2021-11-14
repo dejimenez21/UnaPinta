8646d331fc3d210a01c84872dad94c66c0340b12
Abraham Morillo <abrahammorillo777@gmail.com>
2021-11-14 14:24:59 -0400

    [FIX] Login form validation

--- Front/unapintaui/src/components/login/login.js
+++ Front/unapintaui/src/components/login/login.js
@@ -1,88 +1,79 @@
-import React from 'react';
-import axios from 'axios';
-import Swal from 'sweetalert2'
-import withReactContent from 'sweetalert2-react-content'
-import { browserHistory } from 'react-dom';
+import React from "react";
+import axios from "axios";
+import Swal from "sweetalert2";
+import withReactContent from "sweetalert2-react-content";
 
 class Login extends React.Component {
-    constructor(props){
-        super(props)
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            userName: '',
-            password: ''
-        }
+    this.state = {
+      userName: "",
+      password: "",
+    };
+  }
+
+  handleValidation = (e) => {
+    let username = this.state.userName;
+    let password = this.state.password;
+
+    if (username.length == 0|| password.length == 0) {
+      return false;
     }
+  };
+
+  changeHandler = (e) => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  submitHandler = (e) => {
+    const swal = withReactContent(Swal);
+    e.preventDefault();
     
-    handleValidation = (e) => {
-        let username = this.state.userName;
-        let password = this.state.password;
-        let formValid = true;
+    if (this.handleValidation() == false) {
+      swal.fire("Existen campos vacios", "", "warning");
+    } else {
+      axios
+    .post("https://localhost:5001/api/Auth/login", this.state)
+    .then((response) => {
+      swal.fire(`Bienvenido ${response.status}`, "", "success");
+    })
+    .catch((error) => {
+      swal.fire("Credenciales incorrectos", {error}, "error");
+    });
+    }
+  };
 
-        if (!username["userName"] || !password["password"]) {
-            formValid = false;
-        }
-        
-        return formValid;
-    }
-    
-    changeHandler = (e) => {
-        this.setState({[e.target.name]: e.target.value})
-    }
-    
-    submitHandler = (e) => {
-        const swal = withReactContent(Swal)
-        e.preventDefault();
-        if (this.handleValidation() == false) {
-            swal.fire(
-                'Existen campos vacios',
-                '',
-                'warning'
-            );
-        }
-        else{
-            axios.post("https://localhost:44393/api/Auth/login", this.state).then(response => {
-            swal.fire(
-                `Bienvenido ${response}`,
-                'success'
-            );
-            }).catch(error => {
-                swal.fire(
-                    'Algo ha fallado',
-                    {error},
-                    'error'
-                );
-            })   
-        }     
-    }
+  render() {
+    const { userName, password } = this.state;
+    return (
 
-    render(){
-        const{userName, password} = this.state
-        return(
-            <div className="container p-5">
-                <div className="card" style={{boxShadow: "0 4px 8px 0 rgba(0,0,0,0.2)", transition: "0.5s"}}>
-                <div className="card-header text-center">
-                    <p style={{fontfamily: "Roboto", fontSize:"30px", fontWeight: "Bold"}}>Inicio de Sesión</p>
-                </div>
-                    <div className="card-body">
-                        <form onSubmit={this.submitHandler}>
-                            <div className="mb-3">
-                                <input type="text" name="userName" value={userName} onChange={this.changeHandler} className="form-control" placeholder="Ingrese su nombre de usuario"/>
-                            </div>
-                            <br/>
-                            <div className="mb-3">
-                                <input type="password" name="password" value={password} onChange={this.changeHandler} className="form-control" placeholder="Ingrese su contraseña"/>
-                            </div>
-                            <br />
-                            <button className="btn btn-success" type="submit">
-                                Ingresar
-                            </button>
-                        </form>
-                    </div>
-                </div>
-            </div>
-
-        )
-    }
+        <div className="container p-5 mt-lg-5">
+             <div className="card" style={{boxShadow: "0 4px 8px 0 rgba(0,0,0,0.2)", transition: "0.5s", borderRadius: "10px"}}>
+             <div className="card-header text-center">
+                  <p style={{fontfamily: "Roboto", fontSize:"30px", fontWeight: "Bold"}}>Inicio de Sesión</p>
+             </div>
+                 <div className="card-body">
+                     <form onSubmit={this.submitHandler}>
+                         <div className="mb-3">
+                             <input type="text" name="userName" value={userName} onChange={this.changeHandler} className="form-control" placeholder="Ingrese su nombre de usuario"/>
+                         </div>
+                         <br/>
+                         <div className="mb-3">
+                             <input type="password" name="password" value={password} onChange={this.changeHandler} className="form-control" placeholder="Ingrese su contraseña"/>
+                         </div>
+                         <br />
+                         <button className="btn btn-success" type="submit">
+                             Ingresar
+                         </button>
+                     </form>
+                 </div>
+                 <div className="card-footer justify-content-end">
+                     <a href="#">He olvidado mi contraseña</a>
+                 </div>
+             </div>
+         </div>
+    );
+  }
 }
 export default Login;
