2dd36ff169a6053524a14d2f9710da9afa202c12
Abraham Morillo <abrahammorillo777@gmail.com>
2021-11-17 23:07:23 -0400

    [ADD] state to component

--- Front/unapintaui/src/components/userequester/login/login.js
+++ Front/unapintaui/src/components/userequester/login/login.js
@@ -17,7 +17,7 @@ class Login extends React.Component {
     let username = this.state.userName;
     let password = this.state.password;
 
-    if (username.length == 0|| password.length == 0) {
+    if (username.length == 0 || password.length == 0) {
       return false;
     }
   };
@@ -25,76 +25,109 @@ class Login extends React.Component {
   changeHandler = (e) => {
     this.setState({ [e.target.name]: e.target.value });
   };
-  
+
   submitHandler = (e) => {
     const swal = withReactContent(Swal);
     e.preventDefault();
-    
+
     if (this.handleValidation() == false) {
       swal.fire("Existen campos vacios", "", "warning");
     } else {
-      Axios
-    .post("https://localhost:5001/api/Auth/login", this.state)
-    .then((response) => {
-      console.log(response.data);
-      localStorage.setItem('userToken', JSON.stringify(response.data));
-      window.location.href = '/userRequestHistory';
-    })
-    .catch((error) => {
-      swal.fire("Credenciales incorrectos", {error}, "error");
-    });
+      Axios.post("https://localhost:5001/api/Auth/login", this.state)
+        .then((response) => {
+          console.log(response.data);
+          localStorage.setItem("userToken", JSON.stringify(response.data));
+          window.location.href = "/userRequestHistory";
+        })
+        .catch((error) => {
+          swal.fire("Credenciales incorrectos", { error }, "error");
+        });
     }
   };
 
   registerHandler = () => {
     const swal = withReactContent(Swal);
-    swal.fire({title: "", 
-    text: "¿Cómo quieres registrarte?", 
-    icon: "warning", 
-    showCancelButton: true,
-    confirmButtonText: "Donante", 
-    cancelButtonText: "Solicitante"}).then((result) => {
-      if (result.isConfirmed) {
-        window.location.href = '/requesterRegister';
-      }
-
-      if (result.dismiss == swal.DismissReason.cancel) {
-        window.location.href = '/donorRegister';
-      }
-    });
-  }
+    swal
+      .fire({
+        title: "",
+        text: "¿Cómo quieres registrarte?",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonText: "Donante",
+        cancelButtonText: "Solicitante",
+      })
+      .then((result) => {
+        if (result.isConfirmed) {
+          window.location.href = "/donorRegister";
+        }
+        if (result.dismiss == swal.DismissReason.cancel) {
+          window.location.href = "/requesterRegister";
+        }
+      });
+  };
 
   render() {
     const { userName, password } = this.state;
     return (
-        <div className="container p-5 mt-lg-5">
-             <div className="card" style={{boxShadow: "0 4px 8px 0 rgba(0,0,0,0.2)", transition: "0.5s", borderRadius: "10px"}}>
-             <div className="card-header text-center">
-                  <p style={{fontfamily: "Roboto", fontSize:"30px", fontWeight: "Bold"}}>Inicio de Sesión</p>
-             </div>
-                     <form onSubmit={this.submitHandler}>
-                        <div className="card-body">
-                          <div className="mb-3">
-                              <input type="text" name="userName" value={userName} onChange={this.changeHandler} className="form-control" placeholder="Ingrese su nombre de usuario"/>
-                          </div>
-                          <br/>
-                          <div className="mb-3">
-                              <input type="password" name="password" value={password} onChange={this.changeHandler} className="form-control" placeholder="Ingrese su contraseña"/>
-                          </div>
-                          <br />
-                        </div>
-                        <div className="text-center">
-                          <button className="btn btn-success" type="submit">
-                              Ingresar
-                          </button>
-                        </div>
-                        <br/>
-                     </form>
-                 <div className="card-footer justify-content-end">
-                     <a href="#" onClick={this.registerHandler}>Aun no tienes cuenta? Registrate aqui</a>
-                 </div>
-             </div>
-         </div>
+      <div className="container p-5 mt-lg-5">
+        <div
+          className="card"
+          style={{
+            boxShadow: "0 4px 8px 0 rgba(0,0,0,0.2)",
+            transition: "0.5s",
+            borderRadius: "10px",
+          }}
+        >
+          <div className="card-header text-center">
+            <p
+              style={{
+                fontfamily: "Roboto",
+                fontSize: "30px",
+                fontWeight: "Bold",
+              }}
+            >
+              Inicio de Sesión
+            </p>
+          </div>
+          <form onSubmit={this.submitHandler}>
+            <div className="card-body">
+              <div className="mb-3">
+                <input
+                  type="text"
+                  name="userName"
+                  value={userName}
+                  onChange={this.changeHandler}
+                  className="form-control"
+                  placeholder="Ingrese su nombre de usuario"
+                />
+              </div>
+              <br />
+              <div className="mb-3">
+                <input
+                  type="password"
+                  name="password"
+                  value={password}
+                  onChange={this.changeHandler}
+                  className="form-control"
+                  placeholder="Ingrese su contraseña"
+                />
+              </div>
+              <br />
+            </div>
+            <div className="text-center">
+              <button className="btn btn-success" type="submit">
+                Ingresar
+              </button>
+            </div>
+            <br />
+          </form>
+          <div className="card-footer justify-content-end">
+            <a href="#" onClick={this.registerHandler}>
+              Aun no tienes cuenta? Registrate aqui
+            </a>
+          </div>
+        </div>
+      </div>
     );
   }
 }
--- Front/unapintaui/src/components/userequester/register/donorRegister.js
+++ Front/unapintaui/src/components/userequester/register/donorRegister.js
@@ -1,15 +1,143 @@
-import React from 'react';
+import React from "react";
 
 class DonorRegister extends React.Component {
-    constructor(props){
-        super(props)
-    }
+  constructor(props) {
+    super(props);
 
-    render() {
-        return(<div className="container">
-            <h2>Hola</h2>
-        </div>)
-    }
+    this.state = {
+        name: '',
+        lastName: '',
+        sex: '',
+        age: '',
+        bloodType: '',
+        bithDate: '',
+        email: '',
+        password: '',
+        weight: '',
+        phone: '',
+        province: ''
+    };
+  }  
+
+  render() {
+    return (
+      <div className="container">
+        <div className="container p-5 mt-lg-5">
+          <div
+            className="card"
+            style={{
+              boxShadow: "0 4px 8px 0 rgba(0,0,0,0.2)",
+              transition: "0.5s",
+              borderRadius: "10px",
+            }}
+          >
+            <div className="card-header text-center">
+              <p
+                style={{
+                  fontfamily: "Roboto",
+                  fontSize: "30px",
+                  fontWeight: "Bold",
+                }}
+              >
+                Registro de Donante
+              </p>
+            </div>
+            <form onSubmit={this.submitHandler}>
+                <div className="card-body">
+                <div class="row g-3">
+                <div class="col-sm-6">
+                  <label style={{ fontfamily: "Robot" }}>Nombre</label>
+                  <input type="text" value={this.name} class="form-control" />
+                </div>
+                <div class="col-sm-6">
+                  <label style={{ fontfamily: "Robot" }}>Apellidos</label>
+                  <input type="text" value={this.lastName} class="form-control" />
+                </div>
+                <div class="col-sm-6">
+                  <label style={{ fontfamily: "Robot" }}>Sexo</label>
+                  <select class="form-control">
+                    <option value="" disabled selected hidden>
+                      Seleccionar
+                    </option>
+                    <option value="true">M</option>
+                    <option value="false">F</option>
+                  </select>
+                </div>
+                <div class="col-sm-6">
+                  <label style={{ fontfamily: "Robot" }}>Tipo de sangre</label>
+                  <select class="form-control">
+                    <option value="" disabled selected hidden>
+                      Seleccionar
+                    </option>
+                    <option value="1">A+</option>
+                    <option value="2">A-</option>
+                    <option value="3">B+</option>
+                    <option value="4">B-</option>
+                    <option value="5">AB+</option>
+                    <option value="6">AB-</option>
+                    <option value="7">O+</option>
+                    <option value="8">O-</option>
+                  </select>
+                </div>
+                <div class="col-sm-6">
+                  <label style={{ fontfamily: "Robot" }}>
+                    Fecha de nacimiento
+                  </label>
+                  <input type="date" class="form-control" />
+                </div>
+                <div class="col-sm-6">
+                  <label style={{ fontfamily: "Robot" }}>Email</label>
+                  <input type="email" class="form-control" />
+                </div>
+                <div class="col-sm-6">
+                  <label style={{ fontfamily: "Robot" }}>Telefono</label>
+                  <input type="text" id="phonetxt" class="form-control" />
+                </div>
+                <div class="col-sm-6">
+                  <label style={{ fontfamily: "Robot" }}>Usuario</label>
+                  <input type="text" class="form-control" />
+                </div>
+                <div class="col-sm-6">
+                  <label style={{ fontfamily: "Robot" }}>Contraseña</label>
+                  <input
+                    type="password"
+                    id="passwordtxt"
+                    class="form-control"
+                  />
+                </div>
+                <div class="col-sm-6">
+                  <label style={{ fontfamily: "Robot" }}>Peso (Kg)</label>
+                  <input type="number" class="form-control" />
+                </div>
+                <div class="col-sm-6">
+                  <label style={{ fontfamily: "Robot" }}>
+                    Confirmación de contraseña
+                  </label>
+                  <input
+                    type="password"
+                    id="confirmpasswordtxt"
+                    class="form-control"
+                  />
+                </div>
+                <div class="col-sm-6">
+                  <label style={{ fontfamily: "Robot" }}>Provincia</label>
+                  <select id="provincesDropdown" class="form-control"></select>
+                </div>
+              </div>
+              <br />
+              <div class="text-center">
+                <button class="btn btn-danger float-right">
+                  Guardar
+                </button>
+              </div>
+            </div>
+              <br />
+            </form>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default DonorRegister;
