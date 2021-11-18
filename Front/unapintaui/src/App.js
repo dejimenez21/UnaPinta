314aecc45f161b5031cc8fcebe2daa4cf21caa1f
Abraham Morillo <abrahammorillo777@gmail.com>
2021-11-17 23:08:52 -0400

    [FIX] router

--- Front/unapintaui/src/App.js
+++ Front/unapintaui/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Login from './components/userequester/login/login';
 import RequestHistory from './components/userequester/requestsHistory/requestHistory';
 import DonorRegister from './components/userequester/register/donorRegister';
+import RequesterRegister from './components/userequester/register/donorRegister';
 
 class App extends Component {
   render() {
@@ -12,6 +13,7 @@ class App extends Component {
             <Route exact path='/' element={<Login/>} />
             <Route path='/userRequestHistory' element={<RequestHistory/>} />
             <Route path='/donorRegister' element={<DonorRegister/>} />
+            <Route path='/requestRegister' element={<RequesterRegister/>} />
         </Routes>
       </Router>
     );
