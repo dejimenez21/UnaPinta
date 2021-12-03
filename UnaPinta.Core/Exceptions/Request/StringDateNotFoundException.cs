400ef7a81b12482de681b2ce36c4ea3bf105e7db
Daniel Jimenez <d.jimenezr@aduanas.gob.do>
2021-12-03 10:37:07 -0400

    [ADD] New validation exception for StringDateNotFound

---
+++ UnaPinta.Core/Exceptions/Request/StringDateNotFoundException.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnaPinta.Core.Exceptions.Request
+{
+    public class StringDateNotFoundException : BaseDomainException
+    {
+        const string defaultMessage = "The StringDate doesn't exist";
+        public StringDateNotFoundException(string message, int stringDateId) : base(message)
+        {
+            this.StatusCode = 404;
+            this.ErrorCode = "E3005";
+            this.Title = "Intervalo de fecha no existe";
+            this.spMessage = $"La fecha estimada de respuesta con el id \"{stringDateId}\" no existe.";
+        }
+
+        public StringDateNotFoundException(int stringDateId) : this(defaultMessage, stringDateId) { }
+    }
+}
