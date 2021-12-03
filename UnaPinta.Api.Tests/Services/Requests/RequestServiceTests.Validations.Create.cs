6d78a30c681cd8cb7ea525343f93509d77c3d87d
Daniel Jimenez <d.jimenezr@aduanas.gob.do>
2021-12-03 10:38:09 -0400

    [ADD] New test for request creation with a incorrect ResponseDueDate. TEST RESULT --> FAIL

--- UnaPinta.Api.Tests/Services/Requests/RequestServiceTests.Validations.Create.cs
+++ UnaPinta.Api.Tests/Services/Requests/RequestServiceTests.Validations.Create.cs
@@ -1,5 +1,6 @@
 ﻿using Moq;
 using System.Threading.Tasks;
+using UnaPinta.Core.Exceptions.Request;
 using UnaPinta.Core.Exceptions.User;
 using UnaPinta.Data.Entities;
 using UnaPinta.Dto.Models;
@@ -31,6 +32,31 @@ namespace UnaPinta.Api.Tests.Unit.Services.Requests
 
             //then
             await Assert.ThrowsAsync<UserNotFoundException>(() => createRequestTask);
+
+            _logginBrokerMock.Verify(broker => broker.LogError(It.Is(SameDomainExceptionAs(expectedException))), Times.Once);
+            _requestRepositoryMock.Verify(broker => broker.SelectStringDateById(2), Times.Once);
+            _requestRepositoryMock.VerifyNoOtherCalls();
+        }
+
+
+        [Fact]
+        public async Task ShouldThrowValidationExceptionOnCreateWhenResponseDueDateNotFoundAndLogItAsync()
+        {
+            //given
+            RequestCreateDto inputRequest = GetValidRequestCreateDto();
+            string inputUserName = "f.diaz";
+
+            var expectedException = new StringDateNotFoundException(2);
+
+            _provinceServiceMock
+                .Setup(broker => broker.RetrieveProvinceByCode("SD"))
+                .ReturnsAsync(new Province());
+
+            //when
+            Task<Request> createRequestTask = _requestService.CreateRequest(inputRequest, inputUserName);
+
+            //then
+            await Assert.ThrowsAsync<StringDateNotFoundException>(() => createRequestTask);
 
             _logginBrokerMock.Verify(broker => broker.LogError(It.Is(SameDomainExceptionAs(expectedException))), Times.Once);
             _requestRepositoryMock.Verify(broker => broker.SelectStringDateById(2), Times.Once);
