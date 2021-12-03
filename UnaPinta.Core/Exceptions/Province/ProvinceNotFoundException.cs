39e7edaff93c6d97162bee8b2cb40a00b54a87c7
Daniel Jimenez <d.jimenezr@aduanas.gob.do>
2021-12-03 16:39:57 -0400

    [ADD] New exception for province not found scenarios

---
+++ UnaPinta.Core/Exceptions/Province/ProvinceNotFoundException.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnaPinta.Core.Exceptions.Province
+{
+    public class ProvinceNotFoundException : BaseDomainException
+    {
+        const string defaultMessage = "The province doesn't exist";
+        public ProvinceNotFoundException(string message, string provinceCode) : base(message)
+        {
+            this.StatusCode = 404;
+            this.ErrorCode = "E4000";
+            this.Title = "Provincia no existe";
+            this.spMessage = $"La provincia con el código \"{provinceCode}\" no existe.";
+        }
+
+        public ProvinceNotFoundException(string provinceCode) : this(defaultMessage, provinceCode) { }
+    }
+}
