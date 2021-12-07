711cee9f2b3650a4cd5c0b9f96cdbde50a7ea1a4
Daniel Jimenez <d.jimenezr@aduanas.gob.do>
2021-12-07 19:43:54 -0400

    [ADD] Test for missing patient data on request creation. TEST RESULT ---> FAIL

--- UnaPinta.Api.Tests/Services/Requests/RequestServiceTests.Validations.Create.cs
+++ UnaPinta.Api.Tests/Services/Requests/RequestServiceTests.Validations.Create.cs
@@ -1,4 +1,5 @@
 ﻿using Moq;
+using System;
 using System.Threading.Tasks;
 using UnaPinta.Core.Exceptions.Province;
 using UnaPinta.Core.Exceptions.Request;
@@ -89,6 +90,49 @@ namespace UnaPinta.Api.Tests.Unit.Services.Requests
             _requestRepositoryMock.VerifyNoOtherCalls();
             _provinceServiceMock.Verify(broker => broker.RetrieveProvinceByCode(inputRequest.ProvinceCode), Times.Once);
             _provinceServiceMock.VerifyNoOtherCalls();
+        }
+
+        
+        [Theory]
+        [InlineData("Daniel", null, null, false, true, true)]
+        [InlineData("", null, null, true, true, true)]
+        [InlineData(null, null, 2, true, true, false)]
+        [InlineData("Pedro", "2017-12-4", null, false, false, true)]
+        public async Task ShouldThrowValidationExceptionOnCreateWhenPacientInfoIsMissingAndForMeIsFalseAndLogItAsync(
+            string patientName, DateTime patientBirthDate, int? patientBloodType,
+            bool nameResult, bool birthDateResult, bool bloodTypeResult)
+        {
+            //given
+            RequestCreateDto inputRequest = GetMissingPatientInfoRequestCreateDto(false);
+            inputRequest.Name = patientName;
+            inputRequest.BirthDate = patientBirthDate;
+            inputRequest.BloodTypeId = patientBloodType;
+
+            string inputUserName = "f.diaz";
+
+            var expectedException = new PatientDataMissingException(nameResult, birthDateResult, bloodTypeResult);
+
+            _userManagerMock
+                .Setup(broker => broker.FindByNameAsync(inputUserName))
+                .ReturnsAsync(new User { Id = 1, UserName = "dejimenez21" });
+
+            _requestRepositoryMock
+                .Setup(broker => broker.SelectStringDateById(2))
+                .ReturnsAsync(new StringDate());
+
+            _provinceServiceMock
+                .Setup(broker => broker.RetrieveProvinceByCode("SD"))
+                .ReturnsAsync(new Province());
+
+            //when
+            Task<Request> createRequestTask = _requestService.CreateRequest(inputRequest, inputUserName);
+
+            //then
+            await Assert.ThrowsAsync<PatientDataMissingException>(() => createRequestTask);
+
+            _logginBrokerMock.Verify(broker => broker.LogError(It.Is(SameDomainExceptionAs(expectedException))), Times.Once);
+            _requestRepositoryMock.Verify(broker => broker.SelectStringDateById(2), Times.Once);
+            _requestRepositoryMock.VerifyNoOtherCalls();
         }
     }
 }
