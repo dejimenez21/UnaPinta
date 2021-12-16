226ae1ace3f05c3e590af24498dc69b789ba853a
Daniel Jimenez <d.jimenezr@aduanas.gob.do>
2021-12-16 10:11:53 -0400

    [ADD] New test for incompatible bloodtypes in request creation. TEST RESULT ---> FAIL

--- UnaPinta.Api.Tests/Services/Requests/RequestServiceTests.Validations.Create.cs
+++ UnaPinta.Api.Tests/Services/Requests/RequestServiceTests.Validations.Create.cs
@@ -1,10 +1,13 @@
 ﻿using Moq;
 using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
+using UnaPinta.Core.Exceptions.BloodType;
 using UnaPinta.Core.Exceptions.Province;
 using UnaPinta.Core.Exceptions.Request;
 using UnaPinta.Core.Exceptions.User;
 using UnaPinta.Data.Entities;
+using UnaPinta.Dto.Enums;
 using UnaPinta.Dto.Models;
 using Xunit;
 
@@ -98,7 +101,7 @@ namespace UnaPinta.Api.Tests.Unit.Services.Requests
         [InlineData("", null, null, true, true, true)]
         [InlineData(null, null, 2, true, true, false)]
         [InlineData("Pedro", "2017-12-4", null, false, false, true)]
-        public async Task ShouldThrowValidationExceptionOnCreateWhenPacientInfoIsMissingAndForMeIsFalseAndLogItAsync(
+        public async Task ShouldThrowValidationExceptionOnCreateWhenPatientInfoIsMissingAndForMeIsFalseAndLogItAsync(
             string patientName, DateTime patientBirthDate, int? patientBloodType,
             bool nameResult, bool birthDateResult, bool bloodTypeResult)
         {
@@ -129,6 +132,44 @@ namespace UnaPinta.Api.Tests.Unit.Services.Requests
 
             //then
             await Assert.ThrowsAsync<PatientDataMissingException>(() => createRequestTask);
+
+            _logginBrokerMock.Verify(broker => broker.LogError(It.Is(SameDomainExceptionAs(expectedException))), Times.Once);
+            _requestRepositoryMock.Verify(broker => broker.SelectStringDateById(2), Times.Once);
+            _requestRepositoryMock.VerifyNoOtherCalls();
+        }
+
+        [Theory]
+        [InlineData(BloodTypeEnum.ABminus, "AB-", new BloodTypeEnum[] { BloodTypeEnum.ABplus, BloodTypeEnum.Aminus, BloodTypeEnum.Aplus, BloodTypeEnum.Ominus}, new string[] { "AB+", "A+" })]
+        [InlineData(BloodTypeEnum.Oplus, "O+", new BloodTypeEnum[] { BloodTypeEnum.Bplus, BloodTypeEnum.Ominus }, new string[] { "B+" })]
+        [InlineData(BloodTypeEnum.Aplus, "A+", new BloodTypeEnum[] { BloodTypeEnum.Aplus, BloodTypeEnum.Aminus, BloodTypeEnum.Bplus, BloodTypeEnum.Bminus}, new string[] { "B+", "B-" })]
+        [InlineData(BloodTypeEnum.Bminus, "B-", new BloodTypeEnum[] { BloodTypeEnum.ABplus, BloodTypeEnum.Aminus, BloodTypeEnum.Aplus, BloodTypeEnum.Ominus}, new string[] { "AB+", "A-", "A+" })]
+        public async Task ShouldThrowValidationExceptionOnCreateWhenPossibleBloodTypesAreNotCompatibleWithPatientsBloodTypeAndLogItAsync(
+            BloodTypeEnum patienBloodType, string patientBloodTypeString, IEnumerable<BloodTypeEnum> requestedBloodTypes, IEnumerable<string> incompatiblesBloodTypes)
+        {
+            //given
+            RequestCreateDto inputRequest = GetValidRequestCreateDto();
+
+            string inputUserName = "f.diaz";
+
+            var expectedException = new IncompatibleBloodTypesException(incompatiblesBloodTypes, patientBloodTypeString);
+
+            _userManagerMock
+                .Setup(broker => broker.FindByNameAsync(inputUserName))
+                .ReturnsAsync(new User { Id = 1, UserName = "dejimenez21" });
+
+            _requestRepositoryMock
+                .Setup(broker => broker.SelectStringDateById(2))
+                .ReturnsAsync(new StringDate());
+
+            _provinceServiceMock
+                .Setup(broker => broker.RetrieveProvinceByCode("SD"))
+                .ReturnsAsync(new Province());
+
+            //when
+            Task<Request> createRequestTask = _requestService.CreateRequest(inputRequest, inputUserName);
+
+            //then
+            await Assert.ThrowsAsync<IncompatibleBloodTypesException>(() => createRequestTask);
 
             _logginBrokerMock.Verify(broker => broker.LogError(It.Is(SameDomainExceptionAs(expectedException))), Times.Once);
             _requestRepositoryMock.Verify(broker => broker.SelectStringDateById(2), Times.Once);
