18629f71ee81c03aa391ebcee23aac1dd7ce264f
Daniel Jimenez <d.jimenezr@aduanas.gob.do>
2021-12-22 15:51:25 -0400

    Add new exception for invalid birthdates

---
+++ UnaPinta.Core/Exceptions/User/InvalidBirthDateException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnaPinta.Core.Exceptions.User
+{
+    public class InvalidBirthDateException : BaseDomainException
+    {
+        const string defaultMessage = "The birthdate is invalid.";
+        public InvalidBirthDateException(string message) : base(message)
+        {
+            this.StatusCode = 400;
+            this.ErrorCode = "E3005";
+            this.Title = "Fecha de Nacimiento Invalida";
+            this.spMessage = "La fecha de nacimiento ingresada es invalida.";
+        }
+
+        public InvalidBirthDateException() : this(defaultMessage)
+        {
+
+        }
+    }
+}
